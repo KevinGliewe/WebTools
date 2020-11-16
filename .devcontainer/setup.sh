ebea2e5e87913fa8af72b3a0464bed17262746ae
Kevin Gliewe <kevingliewe@gmail.com>
2020-11-16 19:34:18 +0100

    Don't install PowerShell. Ubuntu 20.04 isn't supported for now.

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -12,7 +12,7 @@ apt-get install -y \
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
 ## Install PowerShell Core
-curl -sSL https://raw.githubusercontent.com/PowerShell/PowerShell/master/tools/install-powershell.sh | bash
+#curl -sSL https://raw.githubusercontent.com/PowerShell/PowerShell/master/tools/install-powershell.sh | bash
 
 ## Setup and install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
