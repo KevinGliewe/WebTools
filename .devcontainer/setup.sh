0cafa606cc624f30c02a3044a1e548acf41ff9a7
Kevin Gliewe <kevingliewe@gmail.com>
2020-11-16 19:42:51 +0100

    Use devcontainer from https://github.com/codespaces-examples/dotnetcore

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -11,8 +11,20 @@ apt-get install -y \
 ## Instsall nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
-## Install PowerShell Core
+## Install Docker CE
+curl -fsSL https://get.docker.com | bash
+
+## Update to the latest PowerShell
 #curl -sSL https://raw.githubusercontent.com/PowerShell/PowerShell/master/tools/install-powershell.sh | bash
+
+## Install Azure Functions Core Tools v3
+wget -q https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
+apt-get update
+apt-get install azure-functions-core-tools-3
+
+## Enable local HTTPS for .NET Core
+dotnet dev-certs https --trust
 
 ## Setup and install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
