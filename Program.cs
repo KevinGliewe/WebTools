1d40a96db2fdc11985875b049f26261242f1c816
Kevin Gliewe <kevingliewe@gmail.com>
2021-08-25 18:48:02 +0200

    Add clipboard functionality

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
+using WebTools.Services;
+
 namespace WebTools
 {
     public class Program
@@ -18,6 +20,8 @@ namespace WebTools
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddSingleton<ClipboardService>();
 
             await builder.Build().RunAsync();
         }
